e190831f2f6c5fe1fe92fc60c13511c06c3d6db3
patrickmylius <53932886+patrickmylius@users.noreply.github.com>
2019-09-09 08:28:43 +0200

    Add files via upload

---
+++ Patrick 1 Projekt/src/Patrick_1_Java_Class.java
@@ -0,0 +1,13 @@
+class Patrick_1_Java_Class
+{
+    public static void main(String[] args)
+    {
+        int x = 10; //værdien af x = 1+. Int (Beskriver altid tal)
+        String Patrick="Patrick";
+        int y = 5; //værdien af y = 5
+        System.out.println(x + y + y + " : " + Patrick); //sout, kommando til at udskrive tekst
+        System.out.println("Patrick");
+        System.out.println("Patrick er fucking lækker");
+
+    }
+}
